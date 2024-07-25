3e6283af6fa4046ef4c106354d817a23c0018376
Selkis90 <selkis@gmail.com>
2024-07-24 22:46:36 -0500

    se cambian colores color de fonto

--- app/src/main/java/com/example/myapplication/ui/inicio/ProveedorViewModel.kt
+++ app/src/main/java/com/example/myapplication/ui/inicio/ProveedorViewModel.kt
@@ -7,7 +7,7 @@ import androidx.lifecycle.ViewModel
 class ProveedorViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is Proveedor Fragment"
+        value = ""
     }
     val text: LiveData<String> = _text
 }
